34f94b6570b6eec834d08cd931405e80a4a797e0
Andrew Lee <ALee@Andrews-MacBook-Pro-6.local>
2014-01-19 12:03:12 -0500

    Removed unnecessary javascript

--- js/custom.js
+++ js/custom.js
@@ -35,180 +35,158 @@ $(document).ready(function() {
    }
 
      });
+
+  /** FULLSCREEN IMAGE **/
+
+  function fullscreenImage(){
+            $('#homeFullScreen').css({height:$(window).height()})   
+  }
+
+  $(window).on("resize",function(e){
+
+         if ($('#homeFullScreen').length)
+              {
+                  console.log("working!")
+                  fullscreenImage();
+              }
+      
   });
-
-/** FULLSCREEN IMAGE **/
-
-function fullscreenImage(){
-          $('#homeFullScreen').css({height:$(window).height()})   
-}
-
-$(window).on("resize",function(e){
-
-       if ($('#homeFullScreen').length)
-            {
-                console.log("working!")
-                fullscreenImage();
-            }
-    
-});
 
 // Transition the header to sticky class when you reach about section //
 
-$(function () {
-    $("#about").waypoint(function () {
-        $("#main-nav").toggleClass("sticky")
-    }, {
-        offset: 160
-    })
-});
+  $(function () {
+      $("#about").waypoint(function () {
+          $("#main-nav").toggleClass("sticky")
+      }, {
+          offset: 160
+      })
+  });
 
-// This function is supposed to shrink the size of the nav when you reach a certain point of the page //
 
-$(function(){
-    $('#main_nav').data('size','big');
-});
+  // Music toggle //
 
-$(window).scroll(function(){
-    var $nav = $('#main_nav');
-    if ($('body').scrollTop() > 0) {
-        if ($nav.data('size') == 'big') {
-            $nav.data('size','small').stop().animate({
-                height:'40px'
-            }, 600);
-        }
-    } else {
-        if ($nav.data('size') == 'small') {
-            $nav.data('size','big').stop().animate({
-                height:'100px'
-            }, 600);
-        }  
+  (function ($) {
+
+    "use strict";
+
+    function setCookie(c_name,value){
+      /*
+      // http://stackoverflow.com/questions/1783302/clear-cookies-on-browser-close
+      //var exdate=new Date();
+      //exdate.setDate(exdate.getDate() + 1);
+      var c_value=escape(value) + "; expires="+exdate.toUTCString();
+      */
+      var c_value=escape(value);
+      var __path = '/';
+      document.cookie=c_name + "=" + c_value + ';  path=' + __path;
+      //reload_styleswitcher_box_with_cookies();
     }
-});
 
-
-// Music toggle //
-
-(function ($) {
-
-  "use strict";
-
-  function setCookie(c_name,value){
-    /*
-    // http://stackoverflow.com/questions/1783302/clear-cookies-on-browser-close
-    //var exdate=new Date();
-    //exdate.setDate(exdate.getDate() + 1);
-    var c_value=escape(value) + "; expires="+exdate.toUTCString();
-    */
-    var c_value=escape(value);
-    var __path = '/';
-    document.cookie=c_name + "=" + c_value + ';  path=' + __path;
-    //reload_styleswitcher_box_with_cookies();
-  }
-
-  function getCookie(c_name){
-    var i,x,y,ARRcookies=document.cookie.split(";");
-    for (i=0;i<ARRcookies.length;i++){
-      x=ARRcookies[i].substr(0,ARRcookies[i].indexOf("="));
-      y=ARRcookies[i].substr(ARRcookies[i].indexOf("=")+1);
-      x=x.replace(/^\s+|\s+$/g,"");
-      if (x==c_name) {
-        if( 'false' == y){
-          return false;
+    function getCookie(c_name){
+      var i,x,y,ARRcookies=document.cookie.split(";");
+      for (i=0;i<ARRcookies.length;i++){
+        x=ARRcookies[i].substr(0,ARRcookies[i].indexOf("="));
+        y=ARRcookies[i].substr(ARRcookies[i].indexOf("=")+1);
+        x=x.replace(/^\s+|\s+$/g,"");
+        if (x==c_name) {
+          if( 'false' == y){
+            return false;
+          }
+          return unescape(y);
         }
-        return unescape(y);
       }
-    }
-    return false;
-  }
-
-  function isPossibleToCreateAudioElement(){
-    if( typeof( $('.music-toggle').attr('data-mp3') ) != 'undefined' ){
-      return true;
-    }
-    
-    if( typeof( $('.music-toggle').attr('data-ogg') ) != 'undefined' ){
-      return true;
-    }
-    return false;
-  }
-  
-  function createAudioElement(){
-    if( $('audio.audio-rain').size() > 0 ){
-      return true;
+      return false;
     }
 
-    var audio_element = '';
-
-    if( typeof( $('.music-toggle').attr('data-mp3') ) != 'undefined' ){
-      audio_element += '<source src="' + $('.music-toggle').attr('data-mp3') + '"></source>';
-    }
-    
-    if( typeof( $('.music-toggle').attr('data-ogg') ) != 'undefined' ){
-      audio_element += '<source src="' + $('.music-toggle').attr('data-ogg') + '"></source>';
-    }
-
-    if( '' === audio_element ){
-      // nothing to play
+    function isPossibleToCreateAudioElement(){
+      if( typeof( $('.music-toggle').attr('data-mp3') ) != 'undefined' ){
+        return true;
+      }
+      
+      if( typeof( $('.music-toggle').attr('data-ogg') ) != 'undefined' ){
+        return true;
+      }
       return false;
     }
     
-    audio_element = '<audio class="audio-rain" loop="true" autoplay="true">' + audio_element + '</audio>';
-  
-    $('.music-toggle').parent().append( audio_element );
-  
-    $('audio.audio-rain').each(function(){ this.pause(); });
-    
-    return true;
-  }
-  
-  if( isPossibleToCreateAudioElement() ){
-    
-    if( ( $('.jqres').width() <= 900 ) ) return;
-
-    var music_playing = getCookie('music_playing');
-
-    if( false === music_playing ){
-      // not initialized
-      music_playing = 1;
-    }
-
-    music_playing = 1 * music_playing;
-
-    $('.music-toggle').show();
-
-    if( music_playing ){
-      $('.music-toggle').addClass('music-playing');
-      createAudioElement();
-      $('audio.audio-rain').each(function(){ this.play(); });
-    }else{
-      $('.music-toggle').addClass('music-stopped');
-    }
-
-    $('.music-toggle').click(function(){
-      // $(this).pause(); -> does not work in f.e. Chrome
-      if( $(this).hasClass('music-playing') ){
-        $('audio.audio-rain').each(function(){
-          this.pause();
-        });
-        setCookie('music_playing', 0);
-      }else{
-        createAudioElement();
-        $('audio.audio-rain').each(function(){
-          this.play();
-        });
-        setCookie('music_playing', 1);
+    function createAudioElement(){
+      if( $('audio.audio-rain').size() > 0 ){
+        return true;
       }
 
-      $('.music-toggle').toggleClass('music-playing');
-      $('.music-toggle').toggleClass('music-stopped');
-    });
+      var audio_element = '';
 
-    //hide music-toggle on iPad
+      if( typeof( $('.music-toggle').attr('data-mp3') ) != 'undefined' ){
+        audio_element += '<source src="' + $('.music-toggle').attr('data-mp3') + '"></source>';
+      }
+      
+      if( typeof( $('.music-toggle').attr('data-ogg') ) != 'undefined' ){
+        audio_element += '<source src="' + $('.music-toggle').attr('data-ogg') + '"></source>';
+      }
 
-    if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
-      $('.music-toggle').hide();
+      if( '' === audio_element ){
+        // nothing to play
+        return false;
+      }
+      
+      audio_element = '<audio class="audio-rain" loop="true" autoplay="true">' + audio_element + '</audio>';
+    
+      $('.music-toggle').parent().append( audio_element );
+    
+      $('audio.audio-rain').each(function(){ this.pause(); });
+      
+      return true;
     }
-  }
+    
+    if( isPossibleToCreateAudioElement() ){
+      
+      if( ( $('.jqres').width() <= 900 ) ) return;
 
-}(jQuery));
+      var music_playing = getCookie('music_playing');
+
+      if( false === music_playing ){
+        // not initialized
+        music_playing = 1;
+      }
+
+      music_playing = 1 * music_playing;
+
+      $('.music-toggle').show();
+
+      if( music_playing ){
+        $('.music-toggle').addClass('music-playing');
+        createAudioElement();
+        $('audio.audio-rain').each(function(){ this.play(); });
+      }else{
+        $('.music-toggle').addClass('music-stopped');
+      }
+
+      $('.music-toggle').click(function(){
+        // $(this).pause(); -> does not work in f.e. Chrome
+        if( $(this).hasClass('music-playing') ){
+          $('audio.audio-rain').each(function(){
+            this.pause();
+          });
+          setCookie('music_playing', 0);
+        }else{
+          createAudioElement();
+          $('audio.audio-rain').each(function(){
+            this.play();
+          });
+          setCookie('music_playing', 1);
+        }
+
+        $('.music-toggle').toggleClass('music-playing');
+        $('.music-toggle').toggleClass('music-stopped');
+      });
+
+      //hide music-toggle on iPad
+
+      if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
+        $('.music-toggle').hide();
+      }
+    }
+
+  }(jQuery));
+
+});
